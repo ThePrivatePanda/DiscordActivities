5404c15a8780248c353cb475d480601dfd833d6e
koala9712 <party.mittal@gmail.com>
2022-01-18 17:40:44 +0530

    Works! (I do not know why I was using dropdowns.)

--- main.py
+++ main.py
@@ -1,142 +1,163 @@
-from nextcord import embeds
 import nextcord
 from nextcord.utils import utcnow
-import asyncio
-import aiohttp
-import json
 from nextcord.ext import commands
 from nextcord import Status
 import config
 
-status_dict = {"dnd": Status.dnd, "idle": Status.idle, "online": Status.online, "offline": Status.offline}
+status_dict = {
+    "dnd": Status.dnd,
+    "idle": Status.idle,
+    "online": Status.online,
+    "offline": Status.offline,
+}
 
-class Dropdown(nextcord.ui.Select):
-    def __init__(self):
-
-        options = [
-            nextcord.SelectOption(label='Poker', description='Play Poker!'),
-            nextcord.SelectOption(label='Chess in the park', description='Play Chess!'),
-            nextcord.SelectOption(label='Doodle Crew', description='Basically scribbl.io'),
-            nextcord.SelectOption(label='Letter Tile', description='A game like skrabble!'),
-            nextcord.SelectOption(label='Spellcast', description='A unique game, play it and see!'),
-            nextcord.SelectOption(label='Youtube Together', description='Watch youtube together, everyone can access the controls.'),
-            nextcord.SelectOption(label='Watch Together', description='Watch youtube together, only the host can control.'),
-            nextcord.SelectOption(label='Checkers in the park', description='Play checkers!'),
-            nextcord.SelectOption(label='Wordsnacks', description='Another unique discord VC Game, try it out!'),
-            nextcord.SelectOption(label='Betrayal.io', description='The VC version of the web version.'),
-            nextcord.SelectOption(label='Fishington.io', description='The VC version of the web version.')
-        ]
-
-        super().__init__(placeholder='Choose the game you wish to play.', min_values=1, max_values=1, options=options)
-
-    async def callback(self, interaction: nextcord.Interaction):
-        async with interaction.channel.typing():
-            await interaction.response.send_message(f'discord.gg/{await make(interaction.user)}', ephemeral=False) # ultra broken stuff
-
-class DropdownView(nextcord.ui.View):
-    def __init__(self):
-        self.add_item(Dropdown())
 
 bot = commands.Bot(
     command_prefix=config.prefix,
+    intents=nextcord.Intents.all(),
     status=status_dict.get(config.status, Status.dnd),
     activity=nextcord.Game(name=config.activity),
-    help_command=None)
+    help_command=None,
+)
 
 app_ids = {
-    1 : 755827207812677713,
-    2 : 832012774040141894,
-    3 : 878067389634314250,
-    4 : 879863686565621790,
-    5 : 852509694341283871,
-    6 : 880218394199220334,
-    7 : 832013003968348200,
-    8 : 879863976006127627,
-    9 : 773336526917861400,
-    10 : 814288819477020702
-} # Missing a(2) game.
+    "Poker": 755827207812677713,
+    "Chess in the park": 832012774040141894,
+    "Doodle Crew": 878067389634314250,
+    "Letter Tile": 879863686565621790,
+    "Spellcast": 852509694341283871,
+    "Awkword": 879863881349087252,
+    "Youtube Together": 755600276941176913,
+    "Watch Together": 880218394199220334,
+    "Checkers in the park": 832013003968348200,
+    "Wordsnacks": 879863976006127627,
+    "Betrayal.io": 773336526917861400,
+    "Fishington.io": 814288819477020702,
+}
 
-async def make(gameid, vcid):
-    url = f"https://discord.com/api/v9/channels/{vcid}/invites"
-    body = {
-        "max_age": 86400,
-        "max_uses": 0,
-        "target_application_id": app_ids[gameid],
-        "target_type": 2,
-        "temporary": False,
-        "validate": None
-    }
-
-    auth = {
-        "Authorization": f"Bot {bot.http.token}",
-        "Content-Type": "application/json",
-    }
-    resp = await bot.session.post(url, data = json.dumps(body, separators=(',', ':'), ensure_ascii=True), headers = auth)
-    code = await resp.json()["code"]
-    return f"https://discord.gg/{code}"
 
 @bot.event
 async def on_ready():
     print("Online")
 
-@bot.command(name="invite")
-async def invite(ctx):
-    await ctx.send("https://discord.com/api/oauth2/authorize?client_id={bot.user.id}&permissions=2251673024&scope=bot%20applications.commands")
 
-@bot.command(name="help")
-async def help(ctx):
-    emb = nextcord.Embed(
-        title="Help command",
-        colour=nextcord.Colour.blue())
+@bot.slash_command(
+    name="invite",
+    guild_ids=[745558385658953848, 881118111967883295],
+    description="Get an invite link to invite me to a server.",
+)
+async def invite(interaction):
+    await interaction.response.send_message(
+        f"https://discord.com/api/oauth2/authorize?client_id={bot.user.id}&permissions=2251673024&scope=bot%20applications.commands"
+    )
 
-    emb.add_field(name="Command `play`:", value=f"""
-1) Poker:`poker`, `pokernight`
+
+@bot.slash_command(
+    name="help",
+    guild_ids=[745558385658953848, 881118111967883295],
+    description="Recieve some help about me",
+)
+async def help(interaction: nextcord.Interaction):
+    emb = nextcord.Embed(title="Help command", colour=nextcord.Colour.blue())
+    emb.add_field(
+        name="Command `play`:",
+        value=f"""
+1) `Poker:`
 -  Play Poker! 
 
-2) Chess in the park: `chess`
+2) `Chess in the park:`
 - Play Chess!
 
-3) Doodle Crew: `skribble`, `doodle`, `dc`
+3) `Doodle Crew:`
 - Basically skribbl.io
 
-4) Letter Tile: `scrabble`, `lt`
+4) `Letter Tile:`
 - A game like scrabble! 
 
-5) `Spellcast`:
-- desc
+5) `Spellcast:`
+- A unique game, play it and see!
 
-6) Youtube Together: `ytt`, `youtube`
+6) `Awkword:`
+- A unique game, play it and see!
+
+7) `Youtube Together:`
 - Everyone can access, i.e. add/remove video, play/pause etc.
 
-7) Watch Together: 'wt', 'watchyt'
+8) `Watch Together:`
 - Only host can access, host can share or hand over the \"remote\".
 
-8) Checkers in the park: `checkers`
+9) `Checkers in the park:`
 - Play Checkers! 
 
-9) `Wordsnacks`: `ws`, `wordsnakes`
+10) `Wordsnacks:`
 -  A unique discord VC Game, try it out!
 
-10) Betrayal.io: `btrio`, `btr`, `betrayal`
+11) `Betrayal.io:`
 -  The VC version of the web version.
 
-11) Fishington.io: `fish`, `fishington`, `ftio`
+11) `Fishington.io:`
 - The VC version of the web version.
 
-""")
-    emb.add_field(name="Usage", value=f"Join a voice channel and type out the command `{config.prefix}play`\nSelect the game you want to play from the **dropdown** and **click on the link the bot responds with**.", inline=False)
-    emb.set_footer(icon_url=ctx.author.avatar.url)
+""",
+    )
+    emb.add_field(
+        name="__Usage:__",
+        value=f"Join a voice channel and type out the command `/play`\nSelect the game you want to play from the **dropdown** and **click on the link the bot responds with**.",
+        inline=False,
+    )
+    emb.add_field(
+        name="Command `invite`:",
+        value="Just run `/invite` to get a link to invite me to a server",
+    )
+    emb.set_footer(icon_url=interaction.user.display_avatar.url)
     emb.set_thumbnail(url=bot.user.avatar.url)
     emb.timestamp = utcnow()
-    await ctx.reply(embed=emb)
+    await interaction.response.send_message(embed=emb, ephemeral=True)
 
 
-# @bot.command(name="play")
-# async def play(ctx, game=None):
+async def make(user, game):
+    invite = await user.voice.channel.create_invite(
+        reason="Play game",
+        unique=False,
+        target_type=2,
+        target_application_id=game,
+    )
+    return invite.url
 
 
-async def startup():
-    bot.session = aiohttp.ClientSession()
+@bot.slash_command(
+    name="play",
+    description="Play a game!",
+    guild_ids=[745558385658953848, 881118111967883295],
+)
+async def play_(
+    interaction: nextcord.Interaction,
+    game: str = nextcord.SlashOption(
+        name="game",
+        choices={
+            "Poker": "755827207812677713",
+            "Chess in the park": "832012774040141894",
+            "Doodle Crew": "878067389634314250",
+            "Letter Tile": "879863686565621790",
+            "Spellcast": "852509694341283871",
+            "Awkword": "879863881349087252",
+            "Youtube Together": "755600276941176913",
+            "Watch Together": "880218394199220334",
+            "Checkers in the park": "832013003968348200",
+            "Wordsnacks": "879863976006127627",
+            "Betryal.io": "773336526917861400",
+            "Fishington.io": "814288819477020702",
+        },
+        description="Choose the game you wish to play!",
+    ),
+):
+    if not interaction.user.voice:
+        return await interaction.response.send_message(
+            "Connect to a voice channel.", ephemeral=True
+        )
+    return await interaction.response.send_message(
+        await make(interaction.user, int(game)), ephemeral=False
+    )
 
-bot.loop.create_task(startup())
+
 bot.run(config.token)
